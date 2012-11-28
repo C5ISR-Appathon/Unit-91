311fa41dddd59b374e96b236c1237223438bffcc
Ken Harding <kenharding@Kens-MacBook-Pro.local>
2012-11-28 09:05:41 -0500

    Committing initial checkin

---
+++ Inventorius/Inventorius/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Inventorius
+//
+//  Created by Ken Harding on 11/28/12.
+//  Copyright (c) 2012 unit91. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
