f92a5dffaef8031d201768ba2659adf9602de3ff
Ken Harding <kenharding@Kens-MacBook-Pro.local>
2012-11-28 09:19:32 -0500

    Adding additional files

---
+++ Inventorius/Inventorius/InventoryDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  Inventorius
+//
+//  Created by Ken Harding on 11/28/12.
+//  Copyright (c) 2012 unit91. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InventoryDetailViewController : UICollectionViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
