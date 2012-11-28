338af8deb6b533bc2aac119cbc81e4576d1af03c
wolfejs <wolfejs@battelle.org>
2012-11-28 11:48:43 -0500

    updated the storyboard and inventory create view controller with functionality similar to the item create view controller

--- Inventorius/Inventorius/InventoryCreateViewController.h
+++ Inventorius/Inventorius/InventoryCreateViewController.h
@@ -10,11 +10,19 @@
 
 #import "Inventory.h"
 
-@interface InventoryCreateViewController : UIViewController
+@interface InventoryCreateViewController : UIViewController <UIImagePickerControllerDelegate>
 
 @property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 
 @property (strong, nonatomic) Inventory *createdInventory;
+
+@property (strong, nonatomic) IBOutlet UIButton *m_cameraButton;
+@property (strong, nonatomic) IBOutlet UITextField *m_nameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *m_quantityTextField;
+@property (strong, nonatomic) IBOutlet UITextField *m_descriptionTextField;
+@property (strong, nonatomic) UIImagePickerController *picker;
+@property (nonatomic, retain) UIImageView* selectedImage;
+- (IBAction)cameraButtonPressed:(id)sender;
 
 @end
