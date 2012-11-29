f0ee89292928536bf1daa9549afb49feb89471a0
Ken Harding <kenharding@Kens-MacBook-Pro.local>
2012-11-28 19:24:32 -0500

    Committing check for if new asset

--- Inventorius/Inventorius/ContainerDetailViewController.m
+++ Inventorius/Inventorius/ContainerDetailViewController.m
@@ -15,7 +15,7 @@
 #import "AssetCreateViewController.h"
 
 @interface ContainerDetailViewController ()
-
+-(void) configureView;
 @end
 
 @implementation ContainerDetailViewController
@@ -27,6 +27,31 @@
         // Custom initialization
     }
     return self;
+}
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem)
+    {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+-(void)configureView
+{    
+    if (self.detailItem)
+    {
+        [self.collectionView reloadData];
+    }
+}
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self configureView];
 }
 
 - (void)viewDidLoad
